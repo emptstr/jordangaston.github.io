a7d279737606e024ba1706c7d423672633ba5ce5
Jordan Gaston <jordangaston@Jordans-MacBook-Air.local>
2024-12-07 16:35:25 -0600

    v2

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const hand = document.querySelector('.waving-hand');
+  
+  function waveOnLoad() {
+    hand.classList.add('wave');
+    setTimeout(function () {
+      hand.classList.remove('wave');
+    }, 2000);
+  }
+
+  setTimeout(function () {
+    waveOnLoad();
+  }, 1000);
+
+
+  hand.addEventListener('mouseover', function () {
+    hand.classList.add('wave');
+  });
+
+  hand.addEventListener('mouseout', function () {
+    hand.classList.remove('wave');
+  });
+});
